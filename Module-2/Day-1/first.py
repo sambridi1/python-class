f486c97050b746c3f28e6a66b4f13281a504bcc5
Sambridhi Shrestha <shresthasambridi771@gmail.com>
2024-01-28 14:57:56 +0545

    list commit

---
+++ Module-2/Day-1/first.py
@@ -0,0 +1,42 @@
+a=[1,2,3,4,5,5,"hello",True]
+print (type(a))
+b=[1,2,3,4,5,"hello",5,True]
+print (type(b))
+print(a[3])
+print(a[-3])
+a.append("sambridhi")
+print(a)
+a.pop()
+print(a)
+a.index(3)
+print(a.index(3))
+a.insert(4,False)
+print(a.insert)
+print(a)
+a.insert(5,'Sam')
+print(a.insert)
+print(a)
+a.insert(a.index(2),'Samm')
+print(a.insert)
+print(a)
+# a.remove(2)
+# print(a.remove)
+# print(a)
+# a.reverse()
+# print(a.reverse)
+# print(a)
+# b=a[1:5]
+# print(b)
+# b=a[:6]
+# print(a)
+# b=a[6:]
+# print(b)
+# b=a[:5:2]
+# print(b)
+
+a.count(5)
+print(a.count(5))
+
+sor=[1,3,9,8,6,6,7]
+sor.sort()
+print(sor)
