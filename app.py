46c93f14dab4d321e6ab30b4dff8d04cef68d024
Sambridhi Shrestha <shresthasambridi771@gmail.com>
2024-03-07 16:24:55 +0545

    sam

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.write("Hello Girls")
+
+st.image('https://images.unsplash.com/photo-1707989327727-10c352b1b299?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNHx8fGVufDB8fHx8fA%3D%3D', caption='Sunrise by the mountains')
+
+
